af061203c4c60a113ee1e528e936e13586407502
RautManisha <manisha.rt@outlook.com>
2022-04-05 23:39:54 +0530

    Adding Sort option

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { getAllLaunchData } from "../lib/launches";
 import Launch from "../components/launch";
+import { useState, useEffect } from "react";
 
 export async function getStaticProps() {
   const launches = await getAllLaunchData();
@@ -10,13 +11,45 @@ export async function getStaticProps() {
     },
   };
 }
+
 export default function Home({ launches }) {
+  const [data, setData] = useState([]);
+  const [sortKey, setSortKey] = useState("id");
+  useEffect(() => {
+    setData(launches);
+  }, []);
+
+  useEffect(() => {
+    setData((data) => {
+      const dataToSort = [...data];
+      // console.log("datatosort: ", dataToSort);
+      dataToSort.sort((a, b) => {
+        if (a[sortKey] < b[sortKey]) {
+          return 1;
+        } else if (a[sortKey] > b[sortKey]) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+      return dataToSort;
+    });
+    // console.log(sortKey);
+    // console.log(data);
+  }, [sortKey]);
+
   return (
     <div>
       <Head>
         <title>Spacex Project</title>
       </Head>
-      <Launch launches={launches} />
+      <label htmlFor="sortBy">Sort By </label>
+      <select id="sortBy" onChange={(e) => setSortKey(e.target.value)}>
+        <option value="id">Id</option>
+        <option value="launch_date_local">Launch Date</option>
+        <option value="mission_name">Mission Name</option>
+      </select>
+      <Launch launches={data} />
     </div>
   );
 }
