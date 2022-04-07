2fb53cfaada4e351b45625d27f568e6392969b11
RautManisha <manisha.rt@outlook.com>
2022-04-07 12:53:04 +0530

    Indefinite scrolling

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,21 @@ export async function getStaticProps() {
 export default function Home({ launches }) {
   const [data, setData] = useState([]);
   const [sortKey, setSortKey] = useState("id");
+  const [isFetching, setIsFetching] = useState(false);
+  const [done, setDone] = useState(false);
+  // const done = false;
   useEffect(() => {
+    // const fetchData = async () => {
+    //   const launches = await getAllLaunchData();
+    //   setData(launches);
+    // };
+    // fetchData();
     setData(launches);
+    window.addEventListener("scroll", handleScroll);
+    console.log("mounted!!!!!!");
+    setDone(false);
+
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   useEffect(() => {
@@ -38,6 +51,35 @@ export default function Home({ launches }) {
     // console.log(data);
   }, [sortKey]);
 
+  useEffect(() => {
+    if (!isFetching) return;
+    const fetchMoreListItems = async () => {
+      console.log("fetch fetch fetch");
+      const moreLaunches = await getAllLaunchData(data.length);
+      if (moreLaunches) {
+        setData([...data, ...moreLaunches]);
+      } else {
+        console.log("Done fetching!!!!!!!!!");
+        setDone(true);
+      }
+      setIsFetching(false);
+    };
+    console.log("done? ", done);
+    if (!done) fetchMoreListItems();
+  }, [isFetching]);
+
+  function handleScroll() {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 <=
+      document.documentElement.offsetHeight
+    )
+      return;
+    // console.log("Fetch more list items!");
+    if (!done) {
+      setIsFetching(true);
+    }
+  }
+
   return (
     <div>
       <Head>
@@ -50,6 +92,8 @@ export default function Home({ launches }) {
         <option value="mission_name">Mission Name</option>
       </select>
       <Launch launches={data} />
+      {/* {console.log(`is fetching ${isFetching}, complete ${isFetchingComplete}`)} */}
+      {isFetching && !done && <h2>Fetching..</h2>}
     </div>
   );
 }
